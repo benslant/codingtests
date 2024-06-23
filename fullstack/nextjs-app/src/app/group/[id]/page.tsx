d9cbcc003437c775a87e2badfc4c2cd1570a2052
Ben Caldwell <ben.caldwell@messagemedia.com>
2024-06-23 15:57:23 +1200

    Got layouts working

--- fullstack/nextjs-app/src/app/group/[id]/page.tsx
+++ fullstack/nextjs-app/src/app/group/[id]/page.tsx
@@ -1,23 +1,27 @@
 import prisma from "../../../lib/db";
 import Link from "next/link";
 
-export default async function GroupsPage({params}) {
+export default async function GroupPage({params}) {
     const group = await prisma.group?.findUnique({
         where: {
             id: String(params.id)
+        },
+        include: {
+            users: true
         }
     });
 
     return (
         <main className="flex flex-col items-center gap-y-5 pt-24 tex-center">
             <h1 className="text-3xl find0semi-bold">{group?.name}</h1>
-
             <ul className="border-t border-b border-black/10 py-5 leading-8">
-                {group?.map((group) => (
-                    <li key={group.id} className="flex items-center justify-between px-5">
-                        <Link href={`/group/${group.id}`}>{group.name}</Link>
-                    </li>
-                ))}
+                {
+                    group.users?.map((user) => 
+                        (
+                            <li key={user.id}>{user.email}</li>
+                        )
+                    )
+                }
             </ul>
         </main>
     )
--- fullstack/nextjs-app/src/app/user/[id]/page.tsx
+++ fullstack/nextjs-app/src/app/user/[id]/page.tsx
@@ -5,12 +5,24 @@ export default async function PostPage({params}) {
         where: {
             id: String(params.id)
         },
+        include: {
+            groups: true
+        }
     })
 
     return (
         <main className="flex flex-col items-center gap-y-5 pt-24 tex-center">
             <h1 className="text-3xl find0semi-bold">{user?.first_name} {user?.last_name}</h1>
             <p>{user?.email}</p>
+            <ul className="border-t border-b border-black/10 py-5 leading-8">
+                {
+                    user?.groups?.map((group) => 
+                        (
+                            <li key={group.id}>{group.name}</li>
+                        )
+                    )
+                }
+            </ul>
         </main>
     );
 }
